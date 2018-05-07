4e881b13b32d1acfc4111bc968c255abe6983f44
Al <lin.alice72@gmail.com>
2018-05-07 16:49:20 -0400

    move image folder to front end

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
 Npc.delete_all
@@ -37,6 +30,7 @@ c8 = DogCollectable.create(name: 'Stick', img: '', location_id: Location.offset(
 c9 = DogCollectable.create(name: 'Dog Tag', img: '', location_id: Location.offset(rand(Location.count)).first.id)
 
 c10 = DogCollectable.create(name: 'Annoyingly Loud Chew Toy', img: '', location_id: Location.offset(rand(Location.count)).first.id)
+
 # c11 = DogCollectable.create(name: 'Soft Chew Toy', img: '', location_id: 205)
 # c12 = DogCollectable.create(name: 'Crinkly Chew Toy', img: '', location_id: 34)
 #
