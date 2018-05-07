969b0c4a6c9ebdb6b3c2424ea51ad0debaa60e78
Al <lin.alice72@gmail.com>
2018-05-07 14:48:53 -0400

    seed adjustment range

--- db/seeds.rb
+++ db/seeds.rb
@@ -53,8 +53,8 @@ c20 = DogCollectable.create(name: 'Lana Del Corgi', img: 'app/assets/images/dogs
 collectables = [c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16, c17, c18, c19, c20]
 
 collectables.each_with_index do |collectable, i|
-  str = rand(0...2)
-  ave = rand(0...2)
+  str = rand(1...5)
+  ave = rand(1...5)
   oldLoc = Location.find(collectable.location_id)
   aindex = east_west.index(oldLoc.ave)
   sindex = north_south.index(oldLoc.street)
