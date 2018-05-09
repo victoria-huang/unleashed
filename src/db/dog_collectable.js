e9db22e6dd06c95ca0964c2b3101f2be6a7c2762
victoria-huang <vctrah@gmail.com>
2018-05-09 09:54:40 -0400

    add inventory, add item to inventory when collected

--- src/db/dog_collectable.js
+++ src/db/dog_collectable.js
@@ -1,14 +1,20 @@
 class DogCollectable {
   constructor(data, location){
     this.id = data.id
-    this.name = data.name
+    this.name = data.name.replace(/your friend/g, '').replace(/,/g, '').replace(/^(a )/g, '')
     this.img = data.img
     this.location = location
+  }
+
+  addToInventory() {
+    const inventory = document.getElementById('collectables')
+    let div = document.createElement('div')
+    div.innerHTML = `<img src=${this.img} width="60" height="50"><br>${this.name}`
+    inventory.appendChild(div)
   }
 
   static removeItem(item) {
     let index = store.dogCollectables.indexOf(item);
     store.dogCollectables.splice(index, 1);
-
   }
 }
--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ window.addEventListener("load", () => {
 document.addEventListener('DOMContentLoaded', () => {
   const navBar = document.getElementById('navbar')
   const modal = document.getElementById('modal-container')
-  const list = document.getElementById('collectables')
+  const inventory = document.getElementById('collectables')
   const textBox = document.getElementById('text')
   const npcBox = document.getElementById('npc')
 
@@ -71,15 +71,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
   navBar.addEventListener('click', (e) => {
     if (e.target.innerText === 'Checklist') {
-      console.log('checklist clicked')
       $('#modal-container').modal('show');
     }
-    console.log(store.dogCollectables)
-    store.dogCollectables.forEach((item) => {
-      let li = document.createElement('li')
-      li.innerText = item.name
-      list.appendChild(li)
-    })
+    // console.log(store.dogCollectables)
+    // store.dogCollectables.forEach((item) => {
+    //   let div = document.createElement('div')
+    //   div.innerHTML = `<img src=${item.img} width="50" height="50"><br>${item.name}`
+    //   inventory.appendChild(div)
+    // })
   })
 
 })
@@ -91,6 +90,7 @@ function checkOnStep(textBox){
   )
   if(item){
     foundItem(item);
+    item.addToInventory();
     DogCollectable.removeItem(item);
   }
 
